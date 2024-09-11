168216918126e4d99e193957dbe265e358bc9d3d
shubham kumar <alphaproph@gmail.com>
2024-09-11 16:05:58 +0530

    added cors setting

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import cloudinary from "cloudinary";
 import { config } from "dotenv";
 
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://frotend-hospital-management-system.vercel.app/', 'https://dashboard-hospital-management-system.vercel.app/login'], // Allow only these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
 
+app.use(cors(corsOptions));
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
