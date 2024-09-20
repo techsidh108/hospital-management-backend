4caa859b013dd00e7a903a85840797425f131920
shubham kumar <alphaproph@gmail.com>
2024-09-21 00:20:42 +0530

    cors changes

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ import mongoose from "mongoose";
 const app = express();
 config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
-
 const corsOptions = {
   origin: ['http://localhost:3000', 'https://frotend-hospital-management-system.vercel.app', 'https://dashboard-hospital-management-system.vercel.app', 'http://localhost:5173', 'http://localhost:5174'], // Allow only these origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
--- server.js
+++ server.js
@@ -10,6 +10,14 @@ cloudinary.v2.config({
 
 config({ path: "./config.env" });
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://frotend-hospital-management-system.vercel.app', 'https://dashboard-hospital-management-system.vercel.app', 'http://localhost:5173', 'http://localhost:5174'], // Allow only these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+app.use(cors(corsOptions));
+
 console.log(process.env.PORT)
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
