5fd9e60132c978a79f8146935ef75576b02be00c
shubham kumar <alphaproph@gmail.com>
2024-09-21 00:37:34 +0530

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
 import { config } from "dotenv";
+import cors from "cors";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
